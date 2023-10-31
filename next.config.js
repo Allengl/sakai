18ccf2ad5a9fd4738d69a1e056106e200ab76dd9
guolong <guolong.deng@hand-china.com>
2023-10-31 10:53:27 +0800

    添加 .env

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',
-  // distDir: 'dists/portal/apps/com.awspaas.user.apps.app20231017165850',
-  // assetPrefix: '/portal/apps/com.awspaas.user.apps.app20231017165850/', //配置静态资源前缀
-  // basePath: '/portal/apps/com.awspaas.user.apps.app20231017165850',    //配置路由跳转时的前缀
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true, //忽略eslint检查
-  },
-  typescript: {
-    ignoreBuildErrors: true, //忽略ts检查
-  },
-  reactStrictMode: true, //严格模式
+
+let nextConfig = {};
+
+if (process.env.NEXT_PUBLIC_MY_ENV === 'production') {
+  nextConfig = {
+    output: 'export',
+    distDir: 'dists/portal/apps/com.awspaas.user.apps.app20231017165850',
+    assetPrefix: '/portal/apps/com.awspaas.user.apps.app20231017165850/', // 配置静态资源前缀
+    basePath: '/portal/apps/com.awspaas.user.apps.app20231017165850', // 配置路由跳转时的前缀
+    trailingSlash: true,
+    eslint: {
+      ignoreDuringBuilds: true, // 忽略 eslint 检查
+    },
+    typescript: {
+      ignoreBuildErrors: true, // 忽略 TypeScript 检查
+    },
+    reactStrictMode: true, // React Strict mode
+  };
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
